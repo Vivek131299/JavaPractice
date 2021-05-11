93b183042aed447b91df271d17552ea57f587770
Vivek Bhavsar <vivek131299@gmail.com>
2021-05-11 15:50:38 +0530

    Notes and Inheritance challenge files

---
+++ Main_Vehicle.java
@@ -0,0 +1,11 @@
+public class Main_Vehicle {
+
+    public static void main(String[] args) {
+
+        Tesla tesla = new Tesla(36);
+        tesla.steer(45);
+        tesla.accelerate(30);
+        tesla.accelerate(20);
+        tesla.accelerate(-42);
+    }
+}
